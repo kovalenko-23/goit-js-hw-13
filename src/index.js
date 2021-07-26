e16a16aefc40b4fedcba71409cd20455194af462
Vitalii Kovalenko <vitaliikovalenko23@gmail.com>
2021-07-26 21:06:10 +0300

    added changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,74 @@
 import './sass/main.scss';
+import API from './partials/fetch-hits.js';
+import cardsTable from './templates/pictures-table.hbs';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import { debounce } from 'lodash';
+
+const DEBOUNCE_DELAY = 300;
+
+const searchInput = document.querySelector('.search-input');
+const gallery = document.querySelector('.gallery');
+const searchBtn = document.querySelector('.search-button');
+const loadMoreBtn = document.querySelector('.load-more');
+let inputValue = '';
+let pageNmb = 1;
+
+searchBtn.setAttribute('disabled', true);
+loadMoreBtn.classList.add('unvisible');
+
+searchInput.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange(event) {
+    inputValue = event.target.value;
+    searchBtn.removeAttribute('disabled');
+    if (!inputValue) {
+        searchBtn.setAttribute('disabled', true);
+    }
+    console.log(inputValue);
+};
+
+searchBtn.addEventListener('click', onButtonClick);
+
+async function onButtonClick(event) {
+    event.preventDefault();
+    try {
+        const responce = await API.fetchHits(inputValue);
+        const hits = responce.hits;
+        const totalHits = responce.totalHits;
+        loadMoreBtn.classList.remove('unvisible');
+
+        if (inputValue) {
+            gallery.innerHTML = '';
+            hits;
+            Notiflix.Notify.success(`Hooray! We found ${totalHits} images!`);
+        }
+
+        if (inputValue.trim('') === '') {
+            return;
+        }
+
+        if (hits.length === 0) {
+             Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        }
+
+        renderCardsTable(hits);
+        console.log(totalHits);
+        console.log(hits);
+    } catch (error) {
+        Notiflix.Notify.failure('OOOOPS');
+    }
+}
+
+function renderCardsTable(hit) {
+    const markup = cardsTable(hit);
+    gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+// loadMoreBtn.addEventListener('click', () => {
+//     pageNmb += 1;
+// });
+
+
+
+
---
+++ src/partials/fetch-hits.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '?key=22617397-addb084a927a6e55ecd24a84b';
+const REQUEST = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
+
+
+async function fetchHits(inputValue, pageNmb) {
+    const responce = await axios.get(`${BASE_URL}${KEY}&q=${inputValue}&${REQUEST}&page=1&}`);
+    const hits = await responce;
+    return hits.data;
+}
+
+export default {fetchHits};
